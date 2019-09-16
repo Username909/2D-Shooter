34cc83cea8934d1e3f5b6c1129a0bf1d4c06bf03
Kimjen Gorospe <kimjengorospe@gmail.com>
2019-09-16 01:33:49 -0400

    Inital Commit
    Started work on the assignment. Created the player and the shooting

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    // Tells the bullet where to go and how fast
+    //Variable Declaration
+    [Header("Bullet Speed")]
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    // Declares the boundary that the player cannot cross
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    // Determines the player movement speed
+    [Header("Movement Settings")]
+    public float speed = 12.0f;
+    public Boundary boundary;
+
+    // Determines the settings for the players bullets and how often it can fire
+    [Header("Attack Settings")]
+    public GameObject bullet;
+    public GameObject bulletSpawn;
+    public float fireRate = 0.5f;
+    private Rigidbody2D rBody;
+
+    private float myTime = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  Determines how fast the player can shoot
+        myTime += Time.deltaTime;
+        if (Input.GetButton("Fire1") && myTime > fireRate)
+        {
+            Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            myTime = 0.0f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Read input
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        //Debug.Log("x: " + horiz + ",y: " + vert);
+
+        Vector2 movement = new Vector2(horiz, vert);
+
+        //Move the Player
+        //Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = movement * speed;
+
+        //Restricts the player from leaving the play area
+        rBody.position = new Vector2(
+            Mathf.Clamp(rBody.position.x, boundary.xMin, boundary.xMax), // Restricts on the X postition to xMin and xMax
+            Mathf.Clamp(rBody.position.y, boundary.yMin, boundary.yMax)); // Restricts on the Y postition to yMin and yMax
+    }
+}
